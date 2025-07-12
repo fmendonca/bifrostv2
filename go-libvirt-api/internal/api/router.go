f4c66f6352f6ddad93014258f94e59564e017a6b
Filipe Caló <filipecm@gmail.com>
2025-07-12 19:43:53 -0300

    update

--- go-libvirt-api/internal/api/router.go
+++ go-libvirt-api/internal/api/router.go
@@ -2,9 +2,13 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
+var db *gorm.DB
+
 func SetupRouter() *gin.Engine {
+	db = database
 	r := gin.Default()
 	SetupRoutes(r)
 	return r
