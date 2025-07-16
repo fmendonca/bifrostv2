47eed85284f31d327083eeb282657db5958b841f
Filipe Mendonça <6471957+fmendonca@users.noreply.github.com>
2025-07-16 17:58:14 -0300

    update project redis

--- bifrost-api/main.go
+++ bifrost-api/main.go
@@ -7,13 +7,12 @@ import (
 
 func main() {
 	InitDB()
+	InitRedis()
 	defer DB.Close()
 
-	InitRedis()
-	defer redisClient.Close()
-
 	http.HandleFunc("/api/v1/vms", VMsHandler)
-	http.HandleFunc("/api/v1/vms/action", VMsActionHandler)
+	http.HandleFunc("/api/v1/vms/", StartVMHandler)
+	http.HandleFunc("/api/v1/vms/", StopVMHandler)
 
 	log.Println("Bifrost API running on port 8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
